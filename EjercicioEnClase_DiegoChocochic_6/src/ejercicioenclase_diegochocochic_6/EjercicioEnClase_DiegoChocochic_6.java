2a63b6402849ab8610a5553a641ebbb1c07e2e18
21DL01-DiegoChocochicL <d.chocochic5bdb@gmail.com>
2026-06-08 13:47:54 -0600

    Add files via upload

---
+++ EjercicioEnClase_DiegoChocochic_6/src/ejercicioenclase_diegochocochic_6/EjercicioEnClase_DiegoChocochic_6.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicioenclase_diegochocochic_6;
+
+/**
+ *
+ * @author LABORATORIO
+ */
+public class EjercicioEnClase_DiegoChocochic_6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
